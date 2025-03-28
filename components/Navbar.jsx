0c039cfae9049e17034b9a13913fec9b1d8b2c2c
Arno Pan <apan11@my.bcit.ca>
2025-03-28 00:03:49 -0700

    first commit; setup project dependencies via vite; started nav and some styles in css.

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav id="navBarTop">
+        <div id="logo">
+            Flexbuster
+        </div>
+        <div>
+            <input type="text" placeholder="Enter Here..."/>
+            <button>Search</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
