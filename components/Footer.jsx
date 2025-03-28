6b658b715eda3ac4caad66d58774f75f82cca785
Arno Pan <apan11@my.bcit.ca>
2025-03-28 00:38:39 -0700

    Add footer component. Add styles. CopyLeft(L) information. Add main font-family Lucida Sans.

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>CopyLeft(L). FlexBusters. For Portfolio Only, by Arno Pan.</div>
+        <div>X (Twitter) | Threads | Instagram | Facebook (Meta)</div>
+    </footer>
+  )
+}
+
+export default Footer
